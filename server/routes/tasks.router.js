7b6d023bf7ccc4a6aa3b0f03357d38be000379f8
Khue Lee <khuelee55@gmail.com>
2022-12-18 19:21:58 -0600

    get function is working and getting data from DATABASE

--- server/routes/tasks.router.js
+++ server/routes/tasks.router.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const pool = require('../modules/pool');
 
 
-// post function to get input from dom
+// post function to get input from DOM
 router.post('/', (req, res) =>{
     console.log('post request made', req.body);
     const newTask = req.body;
@@ -13,17 +13,27 @@ router.post('/', (req, res) =>{
     `;
     // console.log(queryText);
     pool.query(queryText)
-    .then((result =>{
+    .then((result) =>{
         // console.log(result);
         res.sendStatus(201);
-    }))
-
+    })
+    .catch((error) => {
+        res.sendStatus(500);
+    });
 });
 
+// function to get data from database and send to DOM
 router.get('/', (req, res) =>{
     console.log('get request made');
     const queryText = 'SELECT * from tasks';
-    pool.qu
+    pool.query(queryText)
+    .then((result) => {
+        // console.log('result from database:', result);
+        res.send(result.rows);
+    })
+    .catch((error) => {
+        res.sendStatus(500);
+    });
 });
 
 
